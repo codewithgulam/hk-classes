dfd21523057e0c95b174eb953c326a425a3fdb56
Laraib Khan <visitlaraib@gmail.com>
2025-02-18 11:34:14 +0530

    crud

---
+++ app/models.py
@@ -0,0 +1,73 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+class CustomUser(AbstractUser):
+    USER = (
+        (1, 'HOD'),
+        (2, 'STAFF'),
+        (3, 'STUDENT'),
+    )
+    user_type = models.CharField(choices=USER, max_length=50, default=1)
+    profile_pic = models.ImageField(upload_to='media/profile_pic')
+
+
+class School(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.name
+
+class Session_Year(models.Model):
+    session_start = models.CharField(max_length=150)
+    session_end = models.CharField(max_length=150)
+    def __str__(self):
+        return self.session_start + "     To    " + self.session_end
+
+class Student(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address = models.TextField()
+    gender = models.CharField(max_length=100)
+    # branch_id = models.ForeignKey(School, on_delete=models.DO_NOTHING)
+    session_year_id = models.ForeignKey(Session_Year, on_delete=models.DO_NOTHING)
+    join_data = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
+    fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    def __str__(self):
+        return self.admin.first_name + " "+ self.admin.last_name
+
+class Staff(models.Model):
+    admin  = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address = models.TextField()
+    gender = models.CharField(max_length=100)
+    mobile_number = models.CharField(max_length=15)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return self.admin.username
+
+
+
+# class Attendance(models.Model):
+#      = models.ForeignKey(Class, on_delete= models.DO_NOTHING)
+#     attendance_date = models.DateField()
+#     session_year_id = models.ForeignKey(Session_Year, on_delete=models.DO_NOTHING)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.subject_id.name
+
+
+# class Attendance_Report(models.Model):
+#     student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+#     attendance_id = models.ForeignKey(Attendance, on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+
+#     def __str__(self):
+#         return self.student_id.admin.first_name + " " + self.student_id.admin.last_name
